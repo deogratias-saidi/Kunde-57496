f16de08eae3451ca22bfdaa0572017846653a501
Deogratias Saidi <deogratias.saidi@outlook.com>
2024-09-18 14:23:15 +0200

    Refactor gitignore and deployment scripts

--- pipeline-scripts/Deploy-ALZManagementGroups.ps1
+++ pipeline-scripts/Deploy-ALZManagementGroups.ps1
@@ -6,10 +6,10 @@ param (
   [String]$Location = "$($env:LOCATION)",
 
   [Parameter()]
-  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\managementGroups\",
+  [String]$TemplateFile = ".\config\orchestration\managementGroup\managementGroup.main.bicep",
 
   [Parameter()]
-  [String]$TemplateParameterFile = "config\custom-parameters\managementGroups.parameters.all.json",
+  [String]$TemplateParameterFile = ".\config\orchestration\managementGroup\managementGroup.main.parameters.json",
 
   [Parameter()]
   [Boolean]$WhatIfEnabled = [System.Convert]::ToBoolean($($env:IS_PULL_REQUEST))
@@ -21,25 +21,11 @@ if ($NonRootParentManagementGroupId -eq '') {
   $inputObject = @{
     DeploymentName        = -join ('alz-MGDeployment-{0}' -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ'))[0..63]
     Location              = $Location
-    TemplateFile          = $TemplateFile + "managementGroups.bicep"
+    TemplateFile          = $TemplateFile + "managementGroup.main.bicep"
     TemplateParameterFile = $TemplateParameterFile
     WhatIf                = $WhatIfEnabled
     Verbose               = $true
   }
 
   New-AzTenantDeployment @inputObject
-}
-
-if ($NonRootParentManagementGroupId -ne '') {
-  $inputObject = @{
-    ManagementGroupId     = $NonRootParentManagementGroupId
-    DeploymentName        = -join ('alz-MGDeployment-{0}' -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ'))[0..63]
-    Location              = $Location
-    TemplateFile          = $TemplateFile + "managementGroupsScopeEscape.bicep"
-    TemplateParameterFile = $TemplateParameterFile
-    WhatIf                = $WhatIfEnabled
-    Verbose               = $true
-  }
-
-  New-AzManagementGroupDeployment @inputObject
 }
