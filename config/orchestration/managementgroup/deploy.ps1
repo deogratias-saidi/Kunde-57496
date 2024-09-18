0c6aec4210ad10ff4ab5721efb2f509e88d389c0
Deogratias Djuma Saidi <50410476+deogratias-saidi@users.noreply.github.com>
2024-09-18 14:37:59 +0200

    Delete config/orchestration/managementgroup directory

--- config/orchestration/managementgroup/deploy.ps1
+++
@@ -1,9 +0,0 @@
-# For Azure global regions
-
-$inputObject = @{
-    DeploymentName        = -join ('alz-MGDeployment-{0}' -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ'))[0..63]
-    Location              = 'norwayeast'
-    TemplateFile          = "config/orchestration/managementGroup/managementGroup.main.bicep"
-    TemplateParameterFile = 'config/orchestration/managementGroup/managementGroup.main.parameters.json'
-  }
-  New-AzTenantDeployment @inputObject
--- config/orchestration/managementgroup/deployManagementGroup.ps1
+++
@@ -1,8 +0,0 @@
-
-# Construct the deployment command
-$location = "norwayeast"
-$templateFile = ".\config\orchestration\managementgroup\managementGroup.main.bicep"
-$parametersFile = ".\config\orchestration\managementgroup\managementGroup.main.parameters.json"
-
-# Execute the Azure deployment command
-az deployment tenant create --location $location --template-file $templateFile --parameters $parametersFile `
