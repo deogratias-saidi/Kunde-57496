08c3d737ebe03793091c5a79048ebfbe5b46bc84
Deogratias Saidi <deogratias.saidi@outlook.com>
2024-09-20 15:13:55 +0200

    Refactor deployment script: Update workflow trigger and remove unused parameters

--- pipeline-scripts/Deploy-ALZLogging.ps1
+++ pipeline-scripts/Deploy-ALZLogging.ps1
@@ -1,7 +1,8 @@
 param (
     [string]$companyPrefix,
     [string]$parTargetManagementGroupId,
-    [string]$location
+    [string]$location,
+    [switch]$WhatIf
 )
 
 # Determine the correct resource group suffix based on the location
@@ -52,10 +53,27 @@ if (-not (Test-Path "./config/custom-modules/policy/assignments/alzDefaults/alzD
     throw "Bicep template file not found at the specified path."
 }
 
-# Actual deployment
-New-AzManagementGroupDeployment `
+if($whatIf){
+
+    # WhatIf deployment
+
+    Write-Output "Run  the WhatIf for deployment.... "
+
+    New-AzManagementGroupDeployment `
     -ManagementGroupId 'alz' `
     -DeploymentName ("alz-PolicyAssignment-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
     -Location $location `
     -TemplateFile ".\config\custom-modules\policy\assignments\alzDefaults\alzDefaultPolicyAssignments.bicep" `
     -TemplateParameterObject $parameters
+    -WhatIf
+}else {
+    
+    Write-Output "Proceeding with the actual deployment..."
+
+    New-AzManagementGroupDeployment `
+    -ManagementGroupId 'alz' `
+    -DeploymentName ("alz-PolicyAssignment-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
+    -Location $location `
+    -TemplateFile ".\config\custom-modules\policy\assignments\alzDefaults\alzDefaultPolicyAssignments.bicep" `
+    -TemplateParameterObject $parameters
+}
