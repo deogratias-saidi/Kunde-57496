81218b1a39ad3ff5c05fbee720f7b0603032665a
Deogratias Saidi <deogratias.saidi@outlook.com>
2024-09-22 21:50:56 +0200

    Refactor deployment script: Update logging deployment name and template file

--- pipeline-scripts/Deploy-ALZLogging.ps1
+++ pipeline-scripts/Deploy-ALZLogging.ps1
@@ -1,79 +1,43 @@
 param (
-    [string]$companyPrefix,
-    [string]$parTargetManagementGroupId,
+    [string]$parCompanyPrefix,
+    [string]$parPlatManagementSubcriptionId,
     [string]$location,
     [switch]$WhatIf
 )
 
-# Determine the correct resource group suffix based on the location
-if ($location -eq "norwayeast") {
-    $resourceGroupSuffix = "noe"
-} elseif ($location -eq "westeurope") {
-    $resourceGroupSuffix = "weu"
-} elseif ($location -eq "northeurope") {
-    $resourceGroupSuffix = "neu"
-} else {
-    throw "Unsupported location: $location. Only norwayeast, westeurope, and northeurope are allowed."
-}
-
-# Set resource group names dynamically based on the location
-$logAnalyticsWorkspaceResourceGroupName = "rg-$companyPrefix-ecms-$resourceGroupSuffix-logging"
-$logAnalyticsWorkspaceResourceId = "/subscriptions/$parTargetManagementGroupId/resourcegroups/$logAnalyticsWorkspaceResourceGroupName/providers/microsoft.operationalinsights/workspaces/alz-$companyPrefix-log-analytics"
-$dataCollectionRuleVMInsightsResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.Insights/dataCollectionRules/alz-$companyPrefix-ama-vmi-dcr"
-$dataCollectionRuleChangeTrackingResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.Insights/dataCollectionRules/alz-$companyPrefix-ama-ct-dcr"
-$dataCollectionRuleMDFCSQLResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.Insights/dataCollectionRules/alz-$companyPrefix-ama-mdfcsql-dcr"
-$userAssignedManagedIdentityResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.ManagedIdentity/userAssignedIdentities/alz-$companyPrefix-logging-mi"
-
-# Output the resource group names and resource IDs for verification
-Write-Output "Log Analytics Workspace Resource Group Name: $logAnalyticsWorkspaceResourceGroupName"
-Write-Output "Log Analytics Workspace Resource ID: $logAnalyticsWorkspaceResourceId"
-Write-Output "VM Insights Data Collection Rule Resource ID: $dataCollectionRuleVMInsightsResourceId"
-Write-Output "Change Tracking Data Collection Rule Resource ID: $dataCollectionRuleChangeTrackingResourceId"
-Write-Output "MDFCSQL Data Collection Rule Resource ID: $dataCollectionRuleMDFCSQLResourceId"
-Write-Output "User Assigned Managed Identity Resource ID: $userAssignedManagedIdentityResourceId"
 
 # Parameters for deployment
 $parameters = @{
-    parCompanyPrefix = $companyPrefix
-    parLogAnalyticsWorkSpaceAndAutomationAccountLocation = $location
-    parLogAnalyticsWorkspaceResourceId = $logAnalyticsWorkspaceResourceId
-    parDataCollectionRuleVMInsightsResourceId = $dataCollectionRuleVMInsightsResourceId
-    parDataCollectionRuleChangeTrackingResourceId = $dataCollectionRuleChangeTrackingResourceId
-    parDataCollectionRuleMDFCSQLResourceId = $dataCollectionRuleMDFCSQLResourceId
-    parUserAssignedManagedIdentityResourceId = $userAssignedManagedIdentityResourceId
-    parUserAssignedManagedIdentityResourceName = "alz-$companyPrefix-logging-mi"
-    parLogAnalyticsWorkspaceName = "alz-$companyPrefix-log-analytics"
-    parLogAnalyticsWorkspaceResourceGroupName = $logAnalyticsWorkspaceResourceGroupName
-    parLogAnalyticsWorkspaceSubscription = $parTargetManagementGroupId
-    parAutomationAccountName = "alz-$companyPrefix-automation-account"
-    parMsDefenderForCloudEmailSecurityContact = "deogratias.saidi@ecit.no"
+    parCompanyPrefix               = $parCompanyPrefix
+    parPlatManagementSubcriptionId = $parPlatManagementSubcriptionId
+    parLocation                    = $parLocation
+  
 }
 
-if (-not (Test-Path "./config/custom-modules/policy/assignments/alzDefaults/alzDefaultPolicyAssignments.bicep")) {
+# Ensure Bicep template exists
+if (-not (Test-Path ".\config\orchestration\logging\logging.main.bicep")) {
     throw "Bicep template file not found at the specified path."
 }
 
-if($whatIf){
-
-    # WhatIf deployment
-
-    Write-Output "Run  the WhatIf for deployment.... "
-
+# Run WhatIf if the switch is passed
+if ($WhatIf) {
+    Write-Output "Running WhatIf for the deployment..."
     New-AzManagementGroupDeployment `
-    -ManagementGroupId 'alz' `
-    -DeploymentName ("alz-LoggingDeployment-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
-    -Location $location `
-    -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
-    -TemplateParameterObject $parameters
-    -WhatIf
-}else {
-    
+        -ManagementGroupId 'alz' `
+        -DeploymentName ("alz-Logging-WhatIf{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
+        -Location $parLocation `
+        -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
+        -TemplateParameterObject $parameters `
+        -WhatIf
+} 
+else {
+    # Run the actual deployment
     Write-Output "Proceeding with the actual deployment..."
-
     New-AzManagementGroupDeployment `
-    -ManagementGroupId 'alz' `
-    -DeploymentName ("alz-LoggingDeployment-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
-    -Location $location `
-    -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
-    -TemplateParameterObject $parameters
+        -ManagementGroupId 'alz' `
+        -DeploymentName ("alz-Logging-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
+        -Location $parLocation `
+        -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
+        -TemplateParameterObject $parameters
 }
+
--- pipeline-scripts/Deploy-ALZPolicyAssignments.ps1
+++ pipeline-scripts/Deploy-ALZPolicyAssignments.ps1
@@ -1,29 +1,79 @@
 param (
-  [Parameter()]
-  [String]$Location = "$($env:LOCATION)",
-
-  [Parameter()]
-  [String]$TopLevelMGPrefix = "$($env:TOP_LEVEL_MG_PREFIX)",
-
-  [Parameter()]
-  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\policy\assignments\alzDefaults\alzDefaultPolicyAssignments.bicep",
-
-  [Parameter()]
-  [String]$TemplateParameterFile = "config\custom-parameters\alzDefaultPolicyAssignments.parameters.all.json",
-
-  [Parameter()]
-  [Boolean]$WhatIfEnabled = [System.Convert]::ToBoolean($($env:IS_PULL_REQUEST))
+    [string]$companyPrefix,
+    [string]$parTargetManagementGroupId,
+    [string]$location,
+    [switch]$WhatIf
 )
 
-# Parameters necessary for deployment
-$inputObject = @{
-  DeploymentName        = -join ('alz-PolicyAssignmentsDeployment-{0}' -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ'))[0..63]
-  Location              = $Location
-  ManagementGroupId     = $TopLevelMGPrefix
-  TemplateFile          = $TemplateFile
-  TemplateParameterFile = $TemplateParameterFile
-  WhatIf                = $WhatIfEnabled
-  Verbose               = $true
+# Determine the correct resource group suffix based on the location
+if ($location -eq "norwayeast") {
+    $resourceGroupSuffix = "noe"
+} elseif ($location -eq "westeurope") {
+    $resourceGroupSuffix = "weu"
+} elseif ($location -eq "northeurope") {
+    $resourceGroupSuffix = "neu"
+} else {
+    throw "Unsupported location: $location. Only norwayeast, westeurope, and northeurope are allowed."
 }
 
-New-AzManagementGroupDeployment @inputObject
+# Set resource group names dynamically based on the location
+$logAnalyticsWorkspaceResourceGroupName = "rg-$companyPrefix-ecms-$resourceGroupSuffix-logging"
+$logAnalyticsWorkspaceResourceId = "/subscriptions/$parTargetManagementGroupId/resourcegroups/$logAnalyticsWorkspaceResourceGroupName/providers/microsoft.operationalinsights/workspaces/alz-$companyPrefix-log-analytics"
+$dataCollectionRuleVMInsightsResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.Insights/dataCollectionRules/alz-$companyPrefix-ama-vmi-dcr"
+$dataCollectionRuleChangeTrackingResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.Insights/dataCollectionRules/alz-$companyPrefix-ama-ct-dcr"
+$dataCollectionRuleMDFCSQLResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.Insights/dataCollectionRules/alz-$companyPrefix-ama-mdfcsql-dcr"
+$userAssignedManagedIdentityResourceId = "/subscriptions/$parTargetManagementGroupId/resourceGroups/$logAnalyticsWorkspaceResourceGroupName/providers/Microsoft.ManagedIdentity/userAssignedIdentities/alz-$companyPrefix-logging-mi"
+
+# Output the resource group names and resource IDs for verification
+Write-Output "Log Analytics Workspace Resource Group Name: $logAnalyticsWorkspaceResourceGroupName"
+Write-Output "Log Analytics Workspace Resource ID: $logAnalyticsWorkspaceResourceId"
+Write-Output "VM Insights Data Collection Rule Resource ID: $dataCollectionRuleVMInsightsResourceId"
+Write-Output "Change Tracking Data Collection Rule Resource ID: $dataCollectionRuleChangeTrackingResourceId"
+Write-Output "MDFCSQL Data Collection Rule Resource ID: $dataCollectionRuleMDFCSQLResourceId"
+Write-Output "User Assigned Managed Identity Resource ID: $userAssignedManagedIdentityResourceId"
+
+# Parameters for deployment
+$parameters = @{
+    parCompanyPrefix = $parCompanyPrefix
+    parLogAnalyticsWorkSpaceAndAutomationAccountLocation = $location
+    parLogAnalyticsWorkspaceResourceId = $logAnalyticsWorkspaceResourceId
+    parDataCollectionRuleVMInsightsResourceId = $dataCollectionRuleVMInsightsResourceId
+    parDataCollectionRuleChangeTrackingResourceId = $dataCollectionRuleChangeTrackingResourceId
+    parDataCollectionRuleMDFCSQLResourceId = $dataCollectionRuleMDFCSQLResourceId
+    parUserAssignedManagedIdentityResourceId = $userAssignedManagedIdentityResourceId
+    parUserAssignedManagedIdentityResourceName = "alz-$companyPrefix-logging-mi"
+    parLogAnalyticsWorkspaceName = "alz-$companyPrefix-log-analytics"
+    parLogAnalyticsWorkspaceResourceGroupName = $logAnalyticsWorkspaceResourceGroupName
+    parLogAnalyticsWorkspaceSubscription = $parTargetManagementGroupId
+    parAutomationAccountName = "alz-$companyPrefix-automation-account"
+    parMsDefenderForCloudEmailSecurityContact = "deogratias.saidi@ecit.no"
+}
+
+if (-not (Test-Path "./config/custom-modules/policy/assignments/alzDefaults/alzDefaultPolicyAssignments.bicep")) {
+    throw "Bicep template file not found at the specified path."
+}
+
+if($whatIf){
+
+    # WhatIf deployment
+
+    Write-Output "Run  the WhatIf for deployment.... "
+
+    New-AzManagementGroupDeployment `
+    -ManagementGroupId 'alz' `
+    -DeploymentName ("alz-LoggingDeployment-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
+    -Location $location `
+    -TemplateFile ".\config\custom-modules\policy\assignments\alzDefaults\alzDefaultPolicyAssignments.bicep" `
+    -TemplateParameterObject $parameters
+    -WhatIf
+}else {
+    
+    Write-Output "Proceeding with the actual deployment..."
+
+    New-AzManagementGroupDeployment `
+    -ManagementGroupId 'alz' `
+    -DeploymentName ("alz-LoggingDeployment-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
+    -Location $location `
+    -TemplateFile ".\config\custom-modules\policy\assignments\alzDefaults\alzDefaultPolicyAssignments.bicep" `
+    -TemplateParameterObject $parameters
+}
