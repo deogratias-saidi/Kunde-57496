c75c041e071b21d9a3b0e170b6dcea9a88cf5a34
Deogratias Saidi <deogratias.saidi@outlook.com>
2024-09-23 11:04:06 +0200

    Refactor deployment script: Update logging deployment name and template file

--- pipeline-scripts/Deploy-ALZLogging.ps1
+++ pipeline-scripts/Deploy-ALZLogging.ps1
@@ -12,40 +12,31 @@ $parameters = @{
     parLocation                    = $parLocation
 }
 
-# Log the parameters
-Write-Output "Company Prefix: $parCompanyPrefix"
-Write-Output "Management Subscription ID: $parPlatManagementSubcriptionId"
-Write-Output "Location: $parLocation"
-
-# Check and log the current directory
-Write-Output "Current Directory: $(Get-Location)"
 
 # Ensure Bicep template exists
 if (-not (Test-Path ".\config\orchestration\logging\logging.main.bicep")) {
-    Write-Error "Bicep template not found in path: $(Get-Location)\config\orchestration\logging\logging.main.bicep"
     throw "Bicep template file not found."
 }
 
-try {
-    if ($WhatIf) {
-        Write-Output "Running WhatIf for the deployment..."
-        New-AzManagementGroupDeployment `
-            -ManagementGroupId 'alz' `
-            -DeploymentName ("alz-Logging-WhatIf{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
-            -Location $parLocation `
-            -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
-            -TemplateParameterObject $parameters `
-            -WhatIf
-    } else {
-        Write-Output "Proceeding with the actual deployment..."
-        New-AzManagementGroupDeployment `
-            -ManagementGroupId 'alz' `
-            -DeploymentName ("alz-Logging-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
-            -Location $parLocation `
-            -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
-            -TemplateParameterObject $parameters
-    }
-} catch {
-    Write-Error "Deployment failed: $_"
-    throw $_
+
+
+if ($WhatIf) {
+    Write-Output "Running WhatIf for the deployment..."
+    New-AzManagementGroupDeployment `
+        -ManagementGroupId 'alz' `
+        -DeploymentName ("alz-Logging-WhatIf{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
+        -Location $parLocation `
+        -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
+        -TemplateParameterObject $parameters `
+        -WhatIf
 }
+else {
+    Write-Output "Proceeding with the actual deployment..."
+    New-AzManagementGroupDeployment `
+        -ManagementGroupId 'alz' `
+        -DeploymentName ("alz-Logging-{0}" -f (Get-Date -Format 'yyyyMMddTHHMMssffffZ')) `
+        -Location $parLocation `
+        -TemplateFile ".\config\orchestration\logging\logging.main.bicep" `
+        -TemplateParameterObject $parameters
+}
+
